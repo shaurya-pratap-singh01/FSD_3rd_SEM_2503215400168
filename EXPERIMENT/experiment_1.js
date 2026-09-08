e25adfb1ff7b59adfb64698741a70583e5ecc287
Shaurya Pratap Singh <s.p.singh.laptop01@gmail.com>
2026-09-08 12:02:44 +0530

    experiment_1.js

---
+++ EXPERIMENT/experiment_1.js
@@ -0,0 +1,18 @@
+//experiment 1
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+// Register by .on (listener) an event listener for the 'event' event
+myEmitter.on('greet',(name) => {
+    console.log(`Hello, ${name}! Welcome to the event-driven world of Node.js.`);
+})
+
+myEmitter.on('exit', () => {
+    console.log('Goodbye! Thanks for using the event-driven world of Node.js.');
+});
+
+// Emit (TRIGGER) the 'greet' event with a name
+myEmitter.emit('greet', 'dev');
+// Emit the 'exit' event
+myEmitter.emit('exit');
+
